78a888ce5a5f556f3dcdbb5447db2e555685d90a
VRConservation <vance.russell@outlook.com>
2024-08-12 14:00:35 +0100

    adding basemaps to ipyleaflet

--- propo/propo.py
+++ propo/propo.py
@@ -1,12 +1,24 @@
 """Main module."""
 
 import ipyleaflet
+from ipyleaflet import basemaps
 
 class Map(ipyleaflet.Map):
 
     def __init__(self, center=[39, -120], zoom=5, **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
-        self.add_control(ipyleaflet.LayersControl())
-        self.add_control(ipyleaflet.FullScreenControl())
-        self.add_control(ipyleaflet.MeasureControl())
-        self.add_control(ipyleaflet.ScaleControl(position='bottomleft'))
+
+    def add_tile_layer(self, url, name, **kwargs):
+        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add_layer(layer)
+
+    def add_basemap(self, name):
+        
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add(name)    
+
+    def add_layers_control(self, position='topright'):
+        self.add_control(ipyleaflet.LayersControl(position=position))    
