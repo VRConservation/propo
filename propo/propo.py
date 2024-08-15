d4b5a351859f8149619a71a1c8f24c17d7996d69
VRConservation <vance.russell@outlook.com>
2024-08-15 09:13:49 +0100

    uncommented add basemap

--- propo/propo.py
+++ propo/propo.py
@@ -19,34 +19,34 @@ class Map(ipyleaflet.Map):
         """
         super().__init__(center=center, zoom=zoom, **kwargs)
 
-    # def add_tile_layer(self, url, name, **kwargs):
-    #     layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
-    #     self.add_layer(layer)
+    def add_tile_layer(self, url, name, **kwargs):
+        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add_layer(layer)
 
-    # def add_basemap(self, name):
-    #     """
-    #     Adds a basemap to the map.
+    def add_basemap(self, name):
+        """
+        Adds a basemap to the map.
 
-    #     Args:
-    #         name (str or object): The name of the basemap as a string or a basemap object.
+        Args:
+            name (str or object): The name of the basemap as a string or a basemap object.
 
-    #     If the name is a string, it evaluates the corresponding basemap URL and adds it as a tile layer.
-    #     If the name is an object, it directly adds the basemap object.
-    #     """
+        If the name is a string, it evaluates the corresponding basemap URL and adds it as a tile layer.
+        If the name is an object, it directly adds the basemap object.
+        """
 
-    #     if isinstance(name, str):
-    #         url = eval(f"basemaps.{name}").build_url()
-    #         self.add_tile_layer(url, name)
-    #     else:
-    #         self.add(name)    
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add(name)    
 
-    # def add_layers_control(self, position='topright'):
-    #     """
-    #     Adds a layers control to the map.
+    def add_layers_control(self, position='topright'):
+        """
+        Adds a layers control to the map.
 
-    #     Args:
-    #         position (str): The position of the layers control on the map. 
-    #                         Default is 'topright'. Other options include 'topleft', 
-    #                         'bottomleft', and 'bottomright'.
-    #     """
-    #     self.add_control(ipyleaflet.LayersControl(position=position))    
+        Args:
+            position (str): The position of the layers control on the map. 
+                            Default is 'topright'. Other options include 'topleft', 
+                            'bottomleft', and 'bottomright'.
+        """
+        self.add_control(ipyleaflet.LayersControl(position=position))    
